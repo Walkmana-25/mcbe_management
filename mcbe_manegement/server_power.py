72597db1822ed465e9011eb1b4bbef0b48f1b2c4
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-04-26 17:37:07 +0900

    screen 起動処理とserverスタート処理の追加

--- mcbe_manegement/server_power.py
+++ mcbe_manegement/server_power.py
@@ -3,8 +3,6 @@ from msilib.schema import Error
 import subprocess
 import os
 def start(self, option):
-    args = ["screen", "-S", "mcbe_server"]
-    subprocess.check_output(args)#サーバー実行用のscreenを立ち上げる
     
     #最低限必要なファイルが存在しているか確認
     #bedrock_server,server.properties,worldsフォルダ
@@ -13,4 +11,10 @@ def start(self, option):
     worlds_dir = os.path.isdir("../server/worlds")
     if bedrock == True or properties_file == True or worlds_dir == True:
         return "Can not start server. Required file does not exist"
-        
+    
+    #screen の準備
+    args = ["screen", "-dmS", "mcbe_server"]
+    subprocess.check_output(args)#サーバー実行用のscreenを立ち上げる
+
+    server_dir = os.path.abspath("../server/bedrock_server")
+    args = ["screen", "=S", "mcbe_server", "-X", "stuff",f"'LD_LIBRARY_PATH=. {server_dir}'`echo -ne '\015'`" ]
