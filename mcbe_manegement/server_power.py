e431890c367fb2b73596fbcc7f0b39fbcf981fb7
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-04-26 17:23:26 +0900

    起動時の必須ファイル存在確認

--- mcbe_manegement/server_power.py
+++ mcbe_manegement/server_power.py
@@ -1,5 +1,16 @@
+
+from msilib.schema import Error
 import subprocess
+import os
 def start(self, option):
     args = ["screen", "-S", "mcbe_server"]
     subprocess.check_output(args)#サーバー実行用のscreenを立ち上げる
     
+    #最低限必要なファイルが存在しているか確認
+    #bedrock_server,server.properties,worldsフォルダ
+    bedrock = os.path.isfile("../server/bedrock_server")
+    properties_file = os.path.isfile("../server/bedrock_server")
+    worlds_dir = os.path.isdir("../server/worlds")
+    if bedrock == True or properties_file == True or worlds_dir == True:
+        return "Can not start server. Required file does not exist"
+        
