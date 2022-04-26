7240453e574078771b5295e0cfd2e01bc606e727
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-04-26 22:46:06 +0900

    screen上でサーバーを起動させる処理を記述

--- mcbe_manegement/server_power.py
+++ mcbe_manegement/server_power.py
@@ -1,20 +1,28 @@
-
-from msilib.schema import Error
+from ast import arg
+from operator import truediv
 import subprocess
 import os
-def start(self, option):
+
+
+def start(option):
     
     #最低限必要なファイルが存在しているか確認
     #bedrock_server,server.properties,worldsフォルダ
-    bedrock = os.path.isfile("../server/bedrock_server")
-    properties_file = os.path.isfile("../server/bedrock_server")
-    worlds_dir = os.path.isdir("../server/worlds")
-    if bedrock == True or properties_file == True or worlds_dir == True:
-        return "Can not start server. Required file does not exist"
+    bedrock = os.path.isfile("./server/bedrock_server")
+    properties_file = os.path.isfile("./server/bedrock_server")
+    worlds_dir = os.path.isdir("./server/worlds")
+    if not option == "force":
+        if bedrock == False or properties_file == False or worlds_dir == False:
+            return "Can not start server. Required file does not exist"
     
     #screen の準備
+    server_dir = os.path.abspath("./server/")
     args = ["screen", "-dmS", "mcbe_server"]
-    subprocess.check_output(args)#サーバー実行用のscreenを立ち上げる
+    subprocess.check_output(args, cwd=server_dir)#サーバー実行用のscreenを立ち上げる
 
-    server_dir = os.path.abspath("../server/bedrock_server")
-    args = ["screen", "=S", "mcbe_server", "-X", "stuff",f"'LD_LIBRARY_PATH=. {server_dir}'`echo -ne '\015'`" ]
+    #screen上でサーバーを起動させる
+    
+    args = (r"screen -S mcbe_server -X stuff 'LD_LIBRARY_PATH=. ./bedrock_server >> output.txt \n'")
+    result = subprocess.run(args, cwd=server_dir, shell=True)
+
+    return 
