a2634c4694a145d715a85a50fcbfc9eaffbf2266
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-04-27 19:24:38 +0900

    main関数でコマンドを実行できるように修正

--- mcbe_management/__main__.py
+++ mcbe_management/__main__.py
@@ -4,26 +4,29 @@ import server_power
 
 
 import exceptions
-def start(option="normal"):
-    """Start Server(To force activation, use the --force)"""
-    try:
-        ans = server_power.start(option)
-    except exceptions.Required_file_does_not_exist:
-        print("Error:Required File does not exist.", file=sys.stderr)
-    except exceptions.screen_already_exists:
-        print("Error: Server has already started.", file=sys.stderr)
+class server_io(object):
 
-    return ans
+    def start(option="normal"):
+        """Start Server(To force activation, use the --force)"""
+        try:
+            ans = server_power.start(option)
+        except exceptions.Required_file_does_not_exist:
+            print("Error:Required File does not exist.", file=sys.stderr)
+        except exceptions.screen_already_exists:
+            print("Error: Server has already started.", file=sys.stderr)
 
-
-    
-def stop():
-    """Stop Server"""
-    out = server_power.stop()
-    return out
+        return ans
 
 
+
+    def stop():
+        """Stop Server"""
+        out = server_power.stop()
+        return out
+
+def main():
+    fire.Fire(server_io)
 
 
 if __name__ == '__main__':
-    fire.Fire()
+    main()
