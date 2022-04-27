bba07c9ba3928721b09992c26f97a39b28c40054
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-04-27 09:55:03 +0900

    ファイルが存在しないときに標準エラー出力でかえせるようにしたよ

--- mcbe_manegement/__main__.py
+++ mcbe_manegement/__main__.py
@@ -1,13 +1,16 @@
+import sys
 import fire
 import server_power
+
 
 import exceptions
 def start(option="normal"):
     """Start Server(To force activation, use the --force)"""
     try:
         ans = server_power.start(option)
-    except exceptions.Required_file_does_not_exist as ans:
-        pass 
+    except exceptions.Required_file_does_not_exist:
+        print("Error:Required File does not exist.", file=sys.stderr)
+    return 
     
 
 
