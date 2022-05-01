373d1ebba01d204d11f32181c591997a0b91077f
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-05-01 11:03:16 +0900

    chorme, screenがインストールされているか判別する処理を追加

--- mcbe_management/install.py
+++ mcbe_management/install.py
@@ -1,15 +1,28 @@
+import subprocess
 import os 
-from mcbe_management import lib
+from mcbe_management import lib, get_update
+import sys
 
 
 def install():
     #サーバーがインストール済みか判別
     if lib.check_installed() == True:
-        print("MCBE Server Installed")
-        exit()
+        return "MCBE Server Installed"
+        
 
     #Google Chromeがインストール済みか判別
-           
+    try:
+        subprocess.check_output(["google-chrome", "--version"])
+    except subprocess.CalledProcessError:
+        print("Google Chrome is not installed. Please install Google Chrome", file=sys.stderr)
+        exit()
+        
+    #screenがインストール済みか判別
+    try:
+        subprocess.check_output(["screen", "--version"])
+    except subprocess.CalledProcessError:
+        print("screen is not installed. Please install Screen", file=sys.stderr)
+        exit()
 
     
     #/var/games/mcbe/を作成
