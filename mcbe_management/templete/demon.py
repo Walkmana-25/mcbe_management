6f8da928dfa8021a838b70ef7bc67df08d497e03
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-05-29 13:14:53 +0000

    jsonファイルを読み込んでそのデータを変数に読み込む処理を追加

--- mcbe_management/templete/demon.py
+++ mcbe_management/templete/demon.py
@@ -1,5 +1,6 @@
 from mcbe_management import exceptions, server_power, lib
 import sys
+import json
 
 #初期確認を行う
 #serverがインストールされているか確認
@@ -7,7 +8,13 @@ if lib.check_installed() == False:
     print("Server is not installed. Please run 'mcbe install'.", file=sys.stderr)
     sys.exit(1)
 
+#jsonを読み込んで変数に格納する
+with open("/etc/mcbe_manegement.json", "r") as load_config_json:
+    config = json.load(load_config_json)
 
-
-
+auto_update = config["auto_update"]
+auto_fix = config["auto_fix"]
+auto_backup = config["auto_backup"]
+auto_restart = config["auto_restart"]
+discord_bot = config["discord_bot"]
 
