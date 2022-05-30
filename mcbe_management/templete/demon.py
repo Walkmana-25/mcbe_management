82160ac49a3358f2692d56e98ee1566c08737cdf
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-05-30 02:44:01 +0000

    configファイルが存在しなかったら、データファイルからコピーする処理を追加

--- mcbe_management/templete/demon.py
+++ mcbe_management/templete/demon.py
@@ -1,12 +1,26 @@
 from mcbe_management import exceptions, server_power, lib
 import sys
 import json
+import os
+import pkgutil
 
 #初期確認を行う
 #serverがインストールされているか確認
 if lib.check_installed() == False:
     print("Server is not installed. Please run 'mcbe install'.", file=sys.stderr)
     sys.exit(1)
+
+#/etc/mcbe_management.jsonと/var/games/mcbe/script.jsonが存在するか確認 
+#存在しなかったらコピーする
+exist_config = os.path.exits("/etc/mcbe_management.json")
+exsit_script = os.path.exists("/var/games/mcbe/script.json")
+if exist_config == false:
+    with open("/etc/mcbe_management.json", "x") as f:
+        f.write(pkgutil.get_data("mcbe_management", "templete/mcbe_management.json"))
+if exist_script == false:
+    with open("/var/games/mcbe/script.json", "x") as f:
+        f.write(pkgutil.get_data("mcbe_management", "templete/script.json"))
+
 
 #jsonを読み込んで変数に格納する
 with open("/etc/mcbe_manegement.json", "r") as load_config_json:
