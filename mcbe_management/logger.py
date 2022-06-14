c249153570bbf6cd2729d21c6dbb310532d159b6
walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-06-14 11:45:46 +0000

    configにdebug modeを追加 debug modeの時はコンソールに詳細なログを表示する

--- mcbe_management/logger.py
+++ mcbe_management/logger.py
@@ -1,6 +1,17 @@
-from logging import getLogger, StreamHandler, FileHandler, Formatter, ERROR, INFO
+from logging import getLogger, StreamHandler, FileHandler, Formatter, ERROR, INFO, DEBUG
 from datetime import datetime
 import os
+import json
+import re
+#configを読み込んで、debug modeか判別する
+debug_mode = False
+if os.path.exists("/etc/mcbe_management.json") == True:
+    with open("/etc/mcbe_management.json", "r") as load_config_json:
+        text = load_config_json.read()
+        re_text = re.sub(r'/\*[\s\S]*?\*/|//.*', '', text)
+        config = json.loads(re_text)
+        debug_mode = config["debug_mode"]
+
 #log用のディレクトリの生成
 os.makedirs("/var/log/mcbe_management", exist_ok=True)
 #loggerの生成
@@ -12,7 +23,10 @@ handler_format = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #StreamHandlerの設定
 stream_handler = StreamHandler()
 #StreamHandlerはError以上を出力する
-stream_handler.setLevel(ERROR)
+if debug_mode == True:
+    stream_handler.setLevel(DEBUG)
+else:
+    stream_handler.setLevel(ERROR)
 #Formatを登録する
 stream_handler.setFormatter(handler_format)
 
